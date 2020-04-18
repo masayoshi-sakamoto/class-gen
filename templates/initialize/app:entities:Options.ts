536277bcd0b5a38cea4b35c4a6d9559ca48ef265
masayoshi-sakmaoto <masayoshi0222@gmail.com>
2020-04-19 07:07:02 +0900

    一覧ページの実装のデバッグ

--- templates/initialize/app:entities:Options.ts
+++ templates/initialize/app:entities:Options.ts
@@ -1,6 +1,77 @@
-export interface Options {
+export interface IOptionsProps {
   total: number
   page: number
   itemsPerPage: number
   pageCount: number
+  sortBy?: string[]
+  sortDesc?: boolean[]
+  search?: string
+}
+
+export interface IParamsProps {
+  take: number
+  skip: number
+  sort?: string
+  desc?: boolean
+  search?: string
+}
+
+export default class OptionsEntity {
+  private _props: IOptionsProps
+
+  constructor(props: IOptionsProps) {
+    this._props = props
+  }
+
+  get props(): IOptionsProps {
+    return {
+      total: this._props.total,
+      page: this._props.page,
+      itemsPerPage: this._props.itemsPerPage,
+      pageCount: this._props.pageCount,
+      search: this._props.search
+    }
+  }
+
+  get total(): number {
+    return this._props.total
+  }
+
+  get page(): number {
+    return this._props.page
+  }
+
+  get itemsPerPage(): number {
+    return this._props.itemsPerPage
+  }
+
+  get pageCount(): number {
+    return this._props.pageCount
+  }
+
+  get params(): IParamsProps {
+    const page = this._props.page - 1
+    const params: IParamsProps = {
+      take: this._props.itemsPerPage,
+      skip: (page < 0 ? 0 : page) * this._props.itemsPerPage
+    }
+    if (this._props.sortBy && this._props.sortBy[0]) params.sort = this._props.sortBy[0]
+    if (this._props.sortDesc && this._props.sortDesc[0]) params.desc = this._props.sortDesc[0]
+    if (this._props.search) params.search = this._props.search
+    return params
+  }
+}
+
+export const EmptyOptionsPropsFactory = (props?: Partial<IOptionsProps>): IOptionsProps => ({
+  total: 0,
+  page: 0,
+  itemsPerPage: 10,
+  pageCount: 0,
+  sortBy: [],
+  sortDesc: [],
+  ...props
+})
+
+export const EmptyOptionsEntityFactory = (props?: Partial<IOptionsProps>): OptionsEntity => {
+  return new OptionsEntity(EmptyOptionsPropsFactory(props))
 }
---
+++ templates/initialize/app:gateways:appName:translator:options.ts
@@ -0,0 +1,19 @@
+/* eslint camelcase: 0 */
+import { Options } from '@/infrastructure/network/<%= appName %>/schema'
+import { IOptionsProps, EmptyOptionsPropsFactory } from '@/entities/Options'
+
+export const toOptionsProps = (props: Options): IOptionsProps => {
+  if (!props) {
+    return EmptyOptionsPropsFactory()
+  }
+
+  const { total, page, take, count } = props
+  return {
+    total,
+    page,
+    itemsPerPage: take,
+    pageCount: count,
+    sortBy: [],
+    sortDesc: []
+  }
+}
