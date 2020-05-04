98b5d7791d886911f0ed4a87baefc5c727fe7460
masayoshi-sakmaoto <masayoshi0222@gmail.com>
2020-05-04 21:33:46 +0900

    1.4.9

--- templates/initialize/app:gateways:appName:translator:me.ts
+++ templates/initialize/app:gateways:appName:translator:me.ts
@@ -3,9 +3,10 @@ import { Me } from '@/infrastructure/network/<%= appName %>/schema'
 import { IMeProps } from '@/entities/Me'
 
 export const toMeProps = (props: Me): IMeProps => {
-  const { id, name, email } = props
+  const { id, username, name, email } = props
   return {
     id,
+    username,
     name,
     email
   }
--- templates/initialize/app:presenter:fixtures.ts
+++ templates/initialize/app:presenter:fixtures.ts
@@ -1,12 +1,14 @@
 import { RootState } from '@/store'
 import { IAppState } from '@/store/app'
 import { menus } from '@/entities/Menu'
+import { EmptyMeEntityFactory } from '@/entities/Me'
 
 export const AppPropsFactory = (props?: Partial<IAppState>): IAppState => ({
   dialog: {},
   drawer: null,
   errors: {},
   loading: false,
+  me: EmptyMeEntityFactory(),
   menus,
   url: '/',
   ...props
